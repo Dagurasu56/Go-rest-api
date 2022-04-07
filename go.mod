6900ad6296bf331e590ee9037f12b0a4a5062d9d
Douglas Souza <douglas.barbosa@sensedia.com>
2022-04-07 08:26:56 -0300

    criação dos pacotes e primeiro endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Dagurasu56/go-rest-api
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dagurasu56/go-rest-api/models"
+	"github.com/Dagurasu56/go-rest-api/routes"
+)
+
+func main() {
+
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
