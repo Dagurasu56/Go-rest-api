3805561df5c93eb33099d19cfa3690240317ec3b
Douglas Souza <douglas.barbosa@sensedia.com>
2022-04-07 10:02:56 -0300

    criando, editando e excluindo dados

--- routes/routes.go
+++ routes/routes.go
@@ -10,9 +10,12 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", controllers.Home).Methods("Get")
+	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
