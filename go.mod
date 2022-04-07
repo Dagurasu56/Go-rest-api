066ef3972a222320fe17e9985365b38e93484b68
Douglas Souza <douglas.barbosa@sensedia.com>
2022-04-07 11:19:35 -0300

    Middleware e integração com o front-end

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Dagurasu56/go-rest-api
 go 1.13
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"github.com/Dagurasu56/go-rest-api/controllers"
+	"github.com/Dagurasu56/go-rest-api/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
@@ -17,5 +20,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
